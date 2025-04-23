b792ac78180a230b98fc3ef66f5e49f238d15fba
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-04-23 11:49:58 -0300

    feat(app): add components to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+<script setup>
+import Header from './components/Header.vue';
+import Hero from './components/Hero.vue';
+import About from './components/About.vue';
+import Contact from './components/Contact.vue';
+import Footer from './components/Footer.vue';
+import ScrollToTop from './components/ScrollToTop.vue';
+</script>
+
 <template>
-    <h1>App</h1>
+    <Header />
+
+    <main>
+        <Hero />
+        <About />
+        <Contact />
+    </main>
+
+    <Footer />
+
+    <ScrollToTop />
 </template>
