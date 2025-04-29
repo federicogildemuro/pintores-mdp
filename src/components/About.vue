f3a9f4a2f89c3aa82eee8246f5fda94a085b9630
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-04-29 15:52:10 -0300

    feat(about): complete section and add image

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,28 @@
 <template>
-    <section id="nosotros" class="scroll-mt-30 min-h-screen">
-        <h1>Nosotros</h1>
+    <section id="nosotros" class="scroll-mt-30 text-accent text-center m-10">
+        <h1 class="text-3xl sm:text-4xl font-bold mb-10" data-aos="zoom-in">SOBRE NOSOTROS</h1>
+
+        <div class="flex flex-col-reverse lg:flex-row items-center gap-10">
+            <div class="lg:w-1/2 text-lg sm:text-xl">
+                <p class="mb-5" data-aos="fade-up" data-aos-delay="200">
+                    Somos un equipo con experiencia en pintura, ofreciendo soluciones personalizadas y de calidad para
+                    hogares y negocios. Nos apasiona transformar espacios que reflejen tu estilo.
+                </p>
+
+                <p class="mb-5" data-aos="fade-up" data-aos-delay="300">
+                    Te acompañamos en todo el proceso: desde la elección de colores hasta una aplicación cuidada, para
+                    lograr resultados duraderos y profesionales.
+                </p>
+
+                <p class="mb-5" data-aos="fade-up" data-aos-delay="400">
+                    Queremos que tus espacios inspiren y te hagan sentir bien. Trabajamos con dedicación para que cada
+                    proyecto sea único y exitoso.
+                </p>
+            </div>
+
+            <div class="lg:w-1/2" data-aos="fade-up" data-aos-delay="100">
+                <img src="@/assets/about.jpg" alt="Sobre Nosotros" class="border-2 border-primary rounded-lg w-full" />
+            </div>
+        </div>
     </section>
 </template>
