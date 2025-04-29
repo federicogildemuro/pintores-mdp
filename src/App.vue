32363a8e25f9253577551413b2b5c26630a7c6ac
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-04-29 15:34:30 -0300

    feat(aos): install aos and implement it in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { onMounted } from 'vue';
 import Header from './components/Header.vue';
 import Hero from './components/Hero.vue';
 import About from './components/About.vue';
 import Contact from './components/Contact.vue';
 import Footer from './components/Footer.vue';
 import ScrollToTop from './components/ScrollToTop.vue';
+
+onMounted(() => {
+    AOS.init({
+        duration: 1500,
+        once: true,
+    });
+});
 </script>
 
 <template>
