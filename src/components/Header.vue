ad3bd554586d0d582e7dcda6a490ab9a48c3ef4f
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-04-29 15:28:25 -0300

    fix(header): use alias for logo path

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -38,54 +38,34 @@ onMounted(() => {
         <div class="w-full h-full flex items-center justify-between">
             <!-- Logo -->
             <a href="#inicio" class="shrink-0 hover:scale-110 transition-all duration-500 ease-in-out">
-                <img src="../assets/logo.jpg" alt="Logo de Pintores MDP" class="h-24" />
+                <img src="@/assets/logo.jpg" alt="Logo de Pintores MDP" class="h-24" />
             </a>
 
             <!-- Hamburger icon (only visible on small screens) -->
-            <button
-                @click="toggleMenu"
-                class="text-2xl sm:hidden"
-                aria-label="Toggle menu"
-                aria-expanded="isMenuOpen.toString()"
-            >
+            <button @click="toggleMenu" class="text-2xl sm:hidden" aria-label="Toggle menu"
+                aria-expanded="isMenuOpen.toString()">
                 <i :class="isMenuOpen ? 'fas fa-times' : 'fas fa-bars'" aria-hidden="true" />
             </button>
 
             <!-- Navigation Menu -->
-            <nav
-                :class="[
-                    'bg-primary absolute top-full left-0 w-full sm:static sm:block sm:w-auto',
-                    isMenuOpen ? 'block' : 'hidden'
-                ]"
-                role="navigation"
-                aria-label="Main menu"
-            >
+            <nav :class="[
+                'bg-primary absolute top-full left-0 w-full sm:static sm:block sm:w-auto',
+                isMenuOpen ? 'block' : 'hidden'
+            ]" role="navigation" aria-label="Main menu">
                 <ul class="flex flex-col items-center text-center sm:flex-row gap-5 sm:gap-10 p-5 sm:p-0">
                     <li>
-                        <NavLink
-                            href="#inicio"
-                            text="INICIO"
-                            :active="activeSection === 'inicio'"
-                            @navigate="isMenuOpen = false"
-                        />
+                        <NavLink href="#inicio" text="INICIO" :active="activeSection === 'inicio'"
+                            @navigate="isMenuOpen = false" />
                     </li>
 
                     <li>
-                        <NavLink
-                            href="#nosotros"
-                            text="NOSOTROS"
-                            :active="activeSection === 'nosotros'"
-                            @navigate="isMenuOpen = false"
-                        />
+                        <NavLink href="#nosotros" text="NOSOTROS" :active="activeSection === 'nosotros'"
+                            @navigate="isMenuOpen = false" />
                     </li>
 
                     <li>
-                        <NavLink
-                            href="#contacto"
-                            text="CONTACTO"
-                            :active="activeSection === 'contacto'"
-                            @navigate="isMenuOpen = false"
-                        />
+                        <NavLink href="#contacto" text="CONTACTO" :active="activeSection === 'contacto'"
+                            @navigate="isMenuOpen = false" />
                     </li>
                 </ul>
             </nav>
