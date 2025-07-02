0ccad945c479214c88ceaf84d8be2a7f54bb92d9
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-07-02 17:26:25 -0300

    fix(footer): add missing socialLinks file

---
+++ src/data/socialLinks.js
@@ -0,0 +1,24 @@
+const socialLinks = [
+    {
+        name: "Instagram",
+        href: "https://www.instagram.com/pintoresmdp",
+        iconClass: "fab fa-instagram",
+    },
+    {
+        name: "Facebook",
+        href: "https://www.facebook.com/pintoresmdp2018",
+        iconClass: "fab fa-facebook",
+    },
+    {
+        name: "TikTok",
+        href: "https://www.tiktok.com/@pintoresmdp",
+        iconClass: "fab fa-tiktok",
+    },
+    {
+        name: "YouTube",
+        href: "https://www.youtube.com/@pintoresmdp",
+        iconClass: "fab fa-youtube",
+    },
+];
+
+export default socialLinks;
