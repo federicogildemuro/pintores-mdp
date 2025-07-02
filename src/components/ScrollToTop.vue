a5830f9093444136308e3a1818554037a0f83294
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-07-02 17:24:00 -0300

    refactor(scroll-to-top): reorganize component code

--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -1,37 +1,28 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-// State to manage the button visibility
+// Button visibility
 const showButton = ref(false)
-
-// Function to show or hide the button based on scroll position
 function handleScroll() {
     showButton.value = window.scrollY > 200
 }
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll)
+})
 
-// Function to scroll to the top of the page
+// Scroll to top function
 function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth',
     })
 }
-
-// Add event listener for scroll when the component is mounted
-onMounted(() => {
-    window.addEventListener('scroll', handleScroll)
-})
 </script>
 
 <template>
-    <button
-        v-show="showButton"
-        @click="scrollToTop"
+    <button v-show="showButton" @click="scrollToTop"
         class="bg-primary text-light fixed bottom-5 right-5 border-2 px-4 py-3 rounded-full hover:bg-accent transition duration-300 ease-in-out cursor-pointer"
-        title="Ir arriba"
-        aria-label="Scroll to top"
-        :aria-hidden="!showButton"
-    >
+        title="Ir arriba" aria-label="Scroll to top" :aria-hidden="!showButton">
         <i class="fas fa-arrow-up" aria-hidden="true" />
     </button>
 </template>
