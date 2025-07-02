8ee79244508944173c9a8a5ead7531256a8012ed
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-07-02 17:37:35 -0300

    feat(about): update content

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,24 +3,23 @@
         <h1 class="text-3xl sm:text-4xl font-bold mb-10" data-aos="zoom-in">SOBRE NOSOTROS</h1>
 
         <div class="flex flex-col-reverse lg:flex-row items-center gap-10">
-            <div class="lg:w-1/2 text-lg sm:text-xl">
+            <div class="lg:w-1/2 text-lg sm:text-xl text-justify">
                 <p class="mb-5" data-aos="fade-up" data-aos-delay="200">
-                    Somos un equipo con experiencia en pintura, ofreciendo soluciones personalizadas y de calidad para
-                    hogares y negocios. Nos apasiona transformar espacios que reflejen tu estilo.
-                </p>
-
-                <p class="mb-5" data-aos="fade-up" data-aos-delay="300">
-                    Te acompañamos en todo el proceso: desde la elección de colores hasta una aplicación cuidada, para
-                    lograr resultados duraderos y profesionales.
+                    Pintores MDP es una empresa marplatense especializada en <strong>aplicaciones profesionales de
+                        pintura, masilla y revestimientos texturados</strong>. Apostamos a la tecnología con sistemas
+                    mecanizados de última generación que optimizan tiempos, mejoran la calidad y aseguran resultados
+                    superiores.
                 </p>
 
                 <p class="mb-5" data-aos="fade-up" data-aos-delay="400">
-                    Queremos que tus espacios inspiren y te hagan sentir bien. Trabajamos con dedicación para que cada
-                    proyecto sea único y exitoso.
+                    Contamos con <strong>operarios calificados, legalmente habilitados y en constante
+                        formación</strong>. Nos destacamos por nuestra <strong>innovación</strong>, <strong>atención
+                        personalizada</strong> y <strong>comunicación clara</strong>, abordando cada proyecto con
+                    compromiso, seguridad y eficiencia, con un enfoque especial en obras de gran escala o en altura.
                 </p>
             </div>
 
-            <div class="lg:w-1/2" data-aos="fade-up" data-aos-delay="100">
+            <div class="lg:w-1/2" data-aos="fade-up" data-aos-delay="200">
                 <img src="@/assets/about.jpg" alt="Sobre Nosotros" class="border-2 border-primary rounded-lg w-full" />
             </div>
         </div>
