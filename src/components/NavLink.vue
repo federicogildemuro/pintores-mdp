185389b9b079ad67c40fc23b65d56c2167e8781b
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-07-02 17:19:36 -0300

    refactor(navlink): improve component used in header navigation

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,7 +1,7 @@
 <script setup>
 defineProps({
     href: String,
-    text: String,
+    label: String,
     active: Boolean,
 });
 
@@ -9,15 +9,10 @@ const emit = defineEmits(['navigate']);
 </script>
 
 <template>
-    <a
-        :href="href"
-        @click="emit('navigate')"
-        :class="[
-            'pb-2 border-b-2 border-transparent transition-all duration-500 ease-in-out',
-            active ? 'text-accent' : 'text-light hover:text-accent hover:border-accent'
-        ]"
-        :aria-current="active ? 'page' : null"
-    >
-        {{ text }}
+    <a :href="href" @click="emit('navigate')" :class="[
+        'pb-2 border-b-2 border-transparent transition-all duration-500 ease-in-out',
+        active ? 'text-accent hover:border-accent' : 'text-light hover:text-accent hover:border-accent'
+    ]" :aria-current="active ? 'page' : null">
+        {{ label }}
     </a>
 </template>
